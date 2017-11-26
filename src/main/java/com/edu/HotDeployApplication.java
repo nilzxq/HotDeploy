32a0a57acb09eb66d68ec3ea047cec04706487a7
nilzxq <2013358717@qq.com>
2017-11-26 21:39:08 +0800

    springloaded

--- src/main/java/com/edu/HotDeployApplication.java
+++ src/main/java/com/edu/HotDeployApplication.java
@@ -2,7 +2,14 @@ package com.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+* @ClassName: HotDeployApplication 
+* @Description: TODO() 
+* @author zhangxueqing
+* @date 2017年11月26日 下午8:15:50 
+*
+ */
 @SpringBootApplication
 public class HotDeployApplication {
 
---
+++ src/main/java/com/edu/controller/HotDeployController.java
@@ -0,0 +1,23 @@
+package com.edu.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+* @ClassName: HotDeployController 
+* @Description: TODO() 
+* @author zhangxueqing
+* @date 2017年11月26日 下午8:16:49 
+*/
+@Controller
+public class HotDeployController {
+
+	@RequestMapping(value="/say",method=RequestMethod.GET)
+	public String say(HttpServletRequest request) {
+		request.setAttribute("say","Hello world,I am learning spring boot!");
+		return "world";
+	}
+}
